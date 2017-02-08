e26bf28f7144e0edebeae7ca55d424118318d450
ArturPopov <ArturPopov@computing.dundee.ac.uk>
2017-02-08 18:40:28 +0000

    CMakeListst.txt initialized

---
+++ app/src/main/cpp/object_loader.cpp
@@ -0,0 +1,4 @@
+//
+// Created by arturpopov on 08/02/2017.
+//
+
--- app/src/main/java/arturpopov/basicprojectopengles/ObjectLoader.java
+++
@@ -1,9 +0,0 @@
-package arturpopov.basicprojectopengles;
-
-/**
- * Created by arturpopov on 08/02/2017.
- */
-
-class ObjectLoader
-{
-}
