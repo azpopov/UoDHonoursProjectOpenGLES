09d3020cdcac18fb84ef98bb6cf6d3fcac41bec9
ArturPopov <ArturPopov@computing.dundee.ac.uk>
2017-03-02 13:24:49 +0000

    unit tests for many and zero added

--- app/src/test/java/arturpopov/basicprojectopengles/ParticleGeneratorTest.java
+++ app/src/test/java/arturpopov/basicprojectopengles/ParticleGeneratorTest.java
@@ -1,9 +1,16 @@
 package arturpopov.basicprojectopengles;
 
+import android.icu.text.MessagePattern;
+
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.Vector;
 
 import static org.junit.Assert.assertArrayEquals;
 
@@ -30,4 +37,55 @@ public class ParticleGeneratorTest
         List<Particle> actual = gen.particleContainer;
         assertArrayEquals(expected.toArray(), actual.toArray());
     }
+
+    @Test
+    public void sortsReverseWithZeroParticles() throws Exception {
+        ParticleGenerator gen = new ParticleGenerator();
+
+
+        List<Particle> expected = Arrays.asList();
+
+        List<Particle> exampleList = Arrays.asList();
+
+        gen.particleContainer = exampleList;
+
+        gen.sortParticles();
+
+        List<Particle> actual = gen.particleContainer;
+        assertArrayEquals(expected.toArray(), actual.toArray());
+    }
+
+    @Test
+    public void sortsReverseWithManyParticles() throws Exception
+    {
+        ParticleGenerator gen = new ParticleGenerator();
+
+        Random rnd = new Random();
+        rnd.setSeed(12345);
+        List<Particle> expected = new Vector<>();
+        for (int i = 500; i > 0; i--)
+        {
+            Particle particle = new Particle();
+            particle.distanceCamera = (float) i;
+            expected.add(particle);
+        }
+        List<Particle> exampleList = new ArrayList<>(expected);
+        //http://stackoverflow.com/questions/1519736/random-shuffling-of-an-array
+        for (int i = expected.size() - 1; i > 0; i--)
+        {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            Particle a = exampleList.get(index);
+            exampleList.set(index, exampleList.get(i));
+            exampleList.set(i, a);
+        }
+        gen.particleContainer = exampleList;
+
+        gen.sortParticles();
+
+        List<Particle> actual = gen.particleContainer;
+        assertArrayEquals(expected.toArray(), actual.toArray());
+    }
+
+
 }
