d7b54268e4b42b77f55d7627b8b7cea796187bf1
ArturPopov <ArturPopov@computing.dundee.ac.uk>
2017-03-26 12:55:16 +0100

    added cubemap loading functionality

---
+++ app/src/main/java/arturpopov/basicprojectopengles/Skybox.java
@@ -0,0 +1,9 @@
+package arturpopov.basicprojectopengles;
+
+/**
+ * Created by arturpopov on 25/03/2017.
+ */
+
+public class Skybox
+{
+}
--- app/src/main/java/arturpopov/basicprojectopengles/TextureLoader.java
+++ app/src/main/java/arturpopov/basicprojectopengles/TextureLoader.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.opengl.GLES20;
+import android.opengl.GLES30;
 import android.opengl.GLUtils;
 
 /**
@@ -60,4 +61,49 @@ public class TextureLoader
 
         return textureHandle[0];
     }
+
+    public static int loadCubeMap(final Context context, final int[] textureIDs)
+    {
+        final int[] textureHandle = new int[1];
+
+        GLES20.glGenTextures(1, textureHandle, 0);
+        GLES30.glActiveTexture(textureHandle[0]);
+        GLES30.glBindTexture(GLES20.GL_TEXTURE_CUBE_MAP, textureHandle[0]);
+        if (textureHandle[0] != 0)
+        {
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inScaled = false;   // No pre-scaling
+            for(int i = 0; i < 6; i++)
+            {
+                // Read in the resource
+                Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), textureIDs[i], options);
+
+                Matrix mirrorMatrix = new Matrix();
+                mirrorMatrix.preScale(1.0f, -1.0f);
+
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), mirrorMatrix, false);
+
+                // Load the bitmap into the bound texture.
+                GLUtils.texImage2D(GLES20.GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, bitmap, 0);
+
+                // Recycle the bitmap, since its data has been loaded into OpenGL.
+                bitmap.recycle();
+            }
+            GLES30.glTexParameteri(GLES20.GL_TEXTURE_CUBE_MAP, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+            GLES30.glTexParameteri(GLES20.GL_TEXTURE_CUBE_MAP, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+            GLES30.glTexParameteri(GLES20.GL_TEXTURE_CUBE_MAP, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+            GLES30.glTexParameteri(GLES20.GL_TEXTURE_CUBE_MAP, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+            GLES30.glTexParameteri(GLES20.GL_TEXTURE_CUBE_MAP, GLES30.GL_TEXTURE_WRAP_R, GLES20.GL_CLAMP_TO_EDGE);
+
+        }
+
+        if (textureHandle[0] == 0)
+        {
+            throw new RuntimeException("Error loading texture.");
+        }
+        GLES30.glBindTexture(GLES20.GL_TEXTURE_CUBE_MAP, 0);
+        return textureHandle[0];
+    }
+
+
 }
